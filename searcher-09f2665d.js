af23d532ce1d90c9d9894fffc1e1d3fc29dd23bb
santamn <santamn@users.noreply.github.com>
2026-09-11 14:38:19 +0000

    deploy: ac246f27d8e53ccd1ea02649d758713d7581acdb

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-6321f7a8.js',
+                path_to_root + 'searchindex-de4015f7.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
